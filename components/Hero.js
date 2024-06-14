03310a975815c61f3606d7fff4e0595797aa96c6
micic-mihajlo <m2micic@uwaterloo.ca>
2024-06-14 01:04:51 -0400

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
     <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
         <h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">
-          Unlock Your Time`s Full Potential with Dryv
+          Unlock Your Full Potential with Dryv
         </h1>
         <p className="text-lg opacity-80 leading-relaxed">
           Dryv is the AI-powered time management assistant that helps you
